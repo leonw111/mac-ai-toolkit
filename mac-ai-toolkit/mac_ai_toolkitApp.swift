6e5daf8615030767ffbab9f7c88f4df42023fc2c
leonw111 <leigang@anz.io>
2026-01-30 23:01:19 +0800

    Initial Commit

---
+++ mac-ai-toolkit/mac_ai_toolkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_ai_toolkitApp.swift
+//  mac-ai-toolkit
+//
+//  Created by leon on 2026/1/30.
+//
+
+import SwiftUI
+
+@main
+struct mac_ai_toolkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
